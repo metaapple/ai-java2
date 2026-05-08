7554ed672e01bc762206f1e9c09aa13a6b6993fa
metaapple <hera@naver.com>
2026-05-08 17:33:09 +0900

    error있는 코드3

--- src/test/DiaryEntry.java
+++ src/test/DiaryEntry.java
@@ -7,7 +7,7 @@ public class DiaryEntry {
     private String title;
     private String content;
     private Timestamp createdAt;
-    private JLabel3 name; //에러있는 코드
+    //private JLabel3 name; //에러있는 코드
 
 
     public DiaryEntry() {}
